f842c0a08c86bcb74f604e6356ca32733639d3be
Amir-fan <amir.f.fanari@gmail.com>
2025-02-13 22:57:02 +0300

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,6 +1,5 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
@@ -8,27 +7,27 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    // Server settings
     $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';                       //gmail SMTP server set to send through
+    $mail->Host       = 'smtp.gmail.com';   
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'amir.f.fanari@gmail.com';                     //SMTP username (your gmail account)
-    $mail->Password   = '0987654a2014';                               //SMTP password (your gmail password or app password)
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;
+    $mail->Username   = 'amir.f.fanari@gmail.com';  // Your Gmail
+    $mail->Password   = 'exwn udra iobt fykf';  // Use an App Password, NOT your real password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587; 
 
-    //Recipients
-    $mail->setFrom('amir.f.fanari@gmail.com', 'boyfrien');         //Set the sender of the message (your email address)
-    $mail->addAddress('firasfanari963@gmail.com', 'test fanari');     //Add a recipient (your bae's email address)
+    // Recipients
+    $mail->setFrom('amir.f.fanari@gmail.com', 'Amir Fanari');
+    $mail->addAddress('firasfanari963@gmail.com', 'Firas Fanari'); 
 
-    //Content
+    // Content
     $mail->isHTML(true);
-    $mail->Subject = 'I Love You';
-    $mail->Body    = 'Happy Valentine Day My Love. I love you so much. You are the best thing that has ever happened to me ❤';
+    $mail->Subject = 'Test Email from PHPMailer';
+    $mail->Body    = 'Hello Firas, this is a test email from PHPMailer!';
 
     $mail->send();
-    echo 'Message has been sent';
+    echo 'Message has been sent successfully';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+?>
